ba78af6395700d349cb535c05a94ba426e5a3d6b
metchvincecruz <vincecruz06@gmail.com>
2017-02-11 11:56:32 +0800

    With UI

--- app/src/main/java/slu/com/pandora/activity/MainActivity.java
+++ app/src/main/java/slu/com/pandora/activity/MainActivity.java
@@ -2,8 +2,18 @@ package slu.com.pandora.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import slu.com.pandora.R;
+import slu.com.pandora.model.UserResponse;
+import slu.com.pandora.rest.ApiClient;
+import slu.com.pandora.rest.ApiInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +21,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void userLogin(){
+        Button loginBtn = (Button)findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ApiInterface webServiceInterface = ApiClient.getClient().create(ApiInterface.class);
+
+                EditText usernameET = (EditText)findViewById(R.id.usernameET);
+                EditText passwordET = (EditText)findViewById(R.id.passwordET);
+                String username = usernameET.getText().toString();
+                String password = passwordET.getText().toString();
+
+                Call<UserResponse> call = webServiceInterface.login(username, password);
+                call.enqueue(new Callback<UserResponse>() {
+                    @Override
+                    public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                        if (response.isSuccessful()){
+                            if (response.body().getUser().getId() == 1) {
+                                Toast.makeText(MainActivity.this, " Welcome " + response.body().getUser().getName() + "!", Toast.LENGTH_LONG).show();
+                                //homeActivity();
+                            } else {
+                                Toast.makeText(MainActivity.this, " Invalid username or password! ", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, + response.code() + " Failed to login !" + response.errorBody().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserResponse> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage() + " Failed to connect !", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
     }
 }
--- app/src/main/java/slu/com/pandora/rest/ApiInterface.java
+++ app/src/main/java/slu/com/pandora/rest/ApiInterface.java
@@ -1,8 +1,23 @@
 package slu.com.pandora.rest;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import slu.com.pandora.model.UserResponse;
+
 /**
- * Created by matt on 2/10/2017.
+ * Created by vince on 2/11/2017.
  */
 
-public class ApiInterface {
+public interface ApiInterface {
+
+    @POST("login")
+    @FormUrlEncoded
+    @Headers({
+            "Accept: application/json",
+            "User-Agent: Cofmat"
+    })
+    Call<UserResponse> login(@Field("name") String name, @Field("password") String password);
 }
