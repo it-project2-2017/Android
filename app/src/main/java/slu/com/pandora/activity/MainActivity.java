8c8d45d8c551ce0c0939d4c65a7c378ec9e3d3af
mattsayco <mattsayco@gmail.com>
2017-02-18 15:00:46 +0800

    meron na ung para sa pag display nang orders

--- app/src/main/java/slu/com/pandora/activity/MainActivity.java
+++ app/src/main/java/slu/com/pandora/activity/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample);
+        setContentView(R.layout.display_order);
         getProduct();
     }
 
