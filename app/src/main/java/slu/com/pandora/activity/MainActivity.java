d8567da8fb634a29ecbede8da72f5c1d9ad9766e
mattsayco <mattsayco@gmail.com>
2017-02-21 07:50:54 +0800

    para lang sa screenshot

--- app/src/main/java/slu/com/pandora/activity/MainActivity.java
+++ app/src/main/java/slu/com/pandora/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package slu.com.pandora.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_v1);
         //getProduct();
+    }
+
+
+    public void orderup(View view){
+        Intent intent = new Intent(this,sampleXML.class);
+        startActivity(intent);
     }
 
     public void userLogin(){
--- app/src/main/java/slu/com/pandora/activity/sampleXML.java
+++ app/src/main/java/slu/com/pandora/activity/sampleXML.java
@@ -27,6 +27,6 @@ public class sampleXML extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample);
+        setContentView(R.layout.display_order1);
     }
 }
