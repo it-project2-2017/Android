2b03100f8313d7c84d7fd559bcc63609359c9c34
mattsayco <mattsayco@gmail.com>
2017-03-05 19:28:52 +0800

    nakacomment lahat nung kailangan para mairun ung mga layout

--- app/src/main/java/slu/com/pandora/activity/MainActivity.java
+++ app/src/main/java/slu/com/pandora/activity/MainActivity.java
@@ -2,29 +2,84 @@ package slu.com.pandora.activity;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+//para sa finished products
+/*import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import slu.com.pandora.model.ItemObject;
+import slu.com.pandora.adapter.RecyclerViewAdapter;*/
+
+import java.util.ArrayList;
+import java.util.List;
 
 import slu.com.pandora.R;
+import slu.com.pandora.adapter.RecyclerViewAdapter;
+import slu.com.pandora.model.ItemObject;
 
 public class MainActivity extends AppCompatActivity{
+    LinearLayoutManager lLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Login
+        //setContentView(R.layout.login);
+
+        //OrderLayout
+        setContentView(R.layout.order_layout);
+        //AppBar
+        /*Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+
+        //Current Order
+        /*setContentView(R.layout.current_order);
+        setTitle("Current Oders");
+        //AppBar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+
+        //Queue and finished
+        /*
         setContentView(R.layout.finished_order);
-        //Action bar
+        setTitle("Finished Orders");
+        //AppBar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //Objects
+        List<ItemObject> rowListItem = getAllItemList();
+        lLayout = new LinearLayoutManager(this);
 
-        //Animation
+        //Recycle View Main Display
+        RecyclerView rView = (RecyclerView)findViewById(R.id.recycler_view);
+        rView.setLayoutManager(lLayout);
 
+        //puting the cardview inside the recyle view
+        RecyclerViewAdapter rcAdapter = new RecyclerViewAdapter(this, rowListItem);
+        rView.setAdapter(rcAdapter);*/
     }
 
     //Adds res/menu/appbar_menu
@@ -49,6 +104,16 @@ public class MainActivity extends AppCompatActivity{
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private List<ItemObject> getAllItemList(){
+        List<ItemObject> allItems = new ArrayList<ItemObject>();
+        allItems.add(new ItemObject("Table No.1","ID","Product Name","Quantity"));
+        allItems.add(new ItemObject("Table No.2","ID","Product Name","Quantity"));
+        allItems.add(new ItemObject("Table No.3","ID","Product Name","Quantity"));
+        allItems.add(new ItemObject("Table No.4","ID","Product Name","Quantity"));
+
+        return allItems;
     }
 
 }
