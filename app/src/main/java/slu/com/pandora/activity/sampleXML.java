fbd5a67dd1abeab4d5dcca02bdac1c45098d6f16
Matt <Matt@yahoo.com>
2017-03-22 21:38:21 +0800

    alam ko na mag access sa webservice pero sample view pa lang gamit ko

--- app/src/main/java/slu/com/pandora/activity/sampleXML.java
+++ app/src/main/java/slu/com/pandora/activity/sampleXML.java
@@ -12,7 +12,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import slu.com.pandora.R;
+import slu.com.pandora.model.Orders;
+import slu.com.pandora.model.ProdList;
+import slu.com.pandora.rest.ApiClient;
+import slu.com.pandora.rest.ApiInterface;
 
 public class sampleXML extends AppCompatActivity {
     private final static String orderStatus = "unpaid";
@@ -29,6 +38,35 @@ public class sampleXML extends AppCompatActivity {
         quantity = (TextView) findViewById(R.id.sampleQuantity);
         prodName.setText("POG BA AKO?");
         quantity.setText("Pogi ako");
+
+        createConnection();
+    }
+
+    private void createConnection(){
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<Orders> call = apiService.getOrders(orderStatus);
+        call.enqueue(new Callback<Orders>() {
+            @Override
+            public void onResponse(Call<Orders> call, Response<Orders> response) {
+                int statusCode = response.code();
+               /* heirarchy
+                Orders = .getOrderList()
+                OrderList = .getList()
+                List = .get(position) eto ung isang order transaction.
+                ProdList = getProdlist();*/
+
+                List<ProdList> prodList = response.body().getOrderList().getList().get(1).getProdlist();
+                //prodList.get(position).getKey() eto ung pagkuha nang isang order.
+                prodName.setText(prodList.get(1).getKey().toString());
+                quantity.setText(prodList.get(1).getValue().toString());
+
+            }
+
+            @Override
+            public void onFailure(Call<Orders> call, Throwable t) {
+
+            }
+        });
     }
 
 
